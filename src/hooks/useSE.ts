315f1bb1701acd1d897413b9ec977a40eee91521
nebular-lab <hiranodaisuke0721@gmail.com>
2025-01-03 23:48:29 +0900

    サウンド変更

--- src/hooks/useSE.ts
+++ src/hooks/useSE.ts
@@ -1,9 +1,9 @@
 import useSound from "use-sound";
 export const useSE = () => {
-  const [passSE] = useSound("/sound/tsumugi-pass.mp3", {
+  const [passSE] = useSound("/sound/pass.mp3", {
     volume: 0.5,
   });
-  const [drawSE] = useSound("/sound/tsumugi-draw.mp3", {
+  const [drawSE] = useSound("/sound/draw.mp3", {
     volume: 0.5,
   });
   return { passSE, drawSE };
